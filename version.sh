4cb76e9c26a7e62911b90f1301e9de905506346d
Harry Chen <harrychen@qnap.com>
2019-09-06 09:56:51 +0800

    Remove unused files

--- version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -d .git ]; then
-    git describe --tags | perl -pe 'chomp; s/-/./; s/-.*//' | tee VERSION
-elif [ -s VERSION ]; then
-    cat VERSION
-else
-    echo -n UNKNOWN
-fi
